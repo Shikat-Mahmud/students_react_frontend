0cdb772561c2532a1385344323523268f1f820cb
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-09-06 18:29:46 +0600

    data fetched in the card

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const StudentCard = () => {
+const StudentCard = ({ student }) => {
     return (
-        <div className='card m-2 p-3 col-md-3 col-lg-3 col-sm-2' style={{ width: '220px', borderRadius: '10px', backgroundColor: '#efefef' }}>
+        <div className='card m-3 p-3 col-md-3 col-lg-3 col-sm-2' style={{ width: '220px', borderRadius: '10px', backgroundColor: '#efefef' }}>
             <img className='rounded w-100' src="https://caps.msu.edu/_assets/images/placeholder/placeholder-400x400.jpg" alt="Student Photo" style={{ height: '200px', objectFit: 'cover' }} />
             <div className="card-body p-0 mt-2">
                 <h5>
-                    Student Name
+                    {student.name}
                 </h5>
                 <span>
-                    Email: student@gmail.com
+                    {student.email}
                 </span> <br />
                 <span>
-                    Phone: 01254756555
+                    {student.phone}
                 </span>
                 <div className="mt-2">
                     <button type='button' className='btn btn-dark'>
